84c62ea1d978ccc14c643af71a4dd751951ea196
Labbi Ahmed <30775026+Labbi-Ahmed@users.noreply.github.com>
2022-10-03 00:33:51 +0600

    trace the button with id and addEventListner on it by js

--- DisplayTag/js.js
+++ DisplayTag/js.js
@@ -1,15 +1,14 @@
-function remove(){
+
+document.getElementById("removbtn").addEventListener('click', () => {
     document.getElementById("remov").style.display="none";
+})
+document.getElementById("hidebtn").addEventListener('click', () => {
+   document.getElementById("hide").style.visibility="hidden";
+})
 
-}
-function hide(){
-    document.getElementById("hide").style.visibility="hidden";
 
-}
-function reset(){
-    document.getElementById("remov").style.display="block";
+document.getElementById("resetbtn").addEventListener('click', () => {
+     document.getElementById("remov").style.display="block";
     document.getElementById("hide").style.visibility="visible";
-}
-
-
+})
 
