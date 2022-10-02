4dad3983c4fa73de213b0fb7f782082eddcd42e2
Labbi Ahmed <30775026+Labbi-Ahmed@users.noreply.github.com>
2022-10-03 00:13:26 +0600

    display property prectice done

---
+++ DisplayTag/js.js
@@ -0,0 +1,15 @@
+function remove(){
+    document.getElementById("remov").style.display="none";
+
+}
+function hide(){
+    document.getElementById("hide").style.visibility="hidden";
+
+}
+function reset(){
+    document.getElementById("remov").style.display="block";
+    document.getElementById("hide").style.visibility="visible";
+}
+
+
+
